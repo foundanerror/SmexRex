aaca6471c27559e212038d0b9eb897e2cb26bf67
SlayerThaDev <75696110+SlayerThaDev@users.noreply.github.com>
2021-01-06 19:13:37 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix = "!", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
