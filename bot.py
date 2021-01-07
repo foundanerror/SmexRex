05d6493c180bee3a8cdde942ce17b737f9cd452a
SlayerThaDev <75696110+SlayerThaDev@users.noreply.github.com>
2021-01-06 20:51:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 client = commands.Bot(command_prefix = "!", intents=discord.Intents.all())
 
@@ -7,13 +7,17 @@ client = commands.Bot(command_prefix = "!", intents=discord.Intents.all())
 async def on_ready():
     print("bot is online")
 
-<<<<<<< HEAD
 
 @client.command()
 async def ping(ctx):
     print(ctx)
     await ctx.send(f'My Ping : {round(client.latency * 1000)}ms')
 
-
+@client.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    await member.ban(reason = reason)
+    
 
 client.run("Nzk2NTExNjE3NzE3NzY0MTQ3.X_Y_QA.UrBZNiRrSNSQ3aL-w2jQZu7r390")
+
