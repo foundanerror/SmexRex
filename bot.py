4194ed4a6ef9e848a0105ce6feaaf3be326ad6b6
SlayerThaDev <75696110+SlayerThaDev@users.noreply.github.com>
2021-01-06 19:09:34 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord 
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("bot is online")
+
+client.run("Nzk2NTExNjE3NzE3NzY0MTQ3.X_Y_QA.UrBZNiRrSNSQ3aL-w2jQZu7r390")
