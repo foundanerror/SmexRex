fb8c716e8f926f97a776799d22f6bd736a561ccb
SlayerThaDev <75696110+SlayerThaDev@users.noreply.github.com>
2021-01-06 19:25:02 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
 import discord 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "!", intents=discord.Intents.all())
+client = commands.Bot(command_prefix = "^", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     print("bot is online")
+
+
+@client.command()
+async def ping(ctx):
+    print(ctx)
+    await ctx.send(f'My Ping : {round(client.latency * 1000)}ms')
+
+
+
 
 client.run("Nzk2NTExNjE3NzE3NzY0MTQ3.X_Y_QA.UrBZNiRrSNSQ3aL-w2jQZu7r390")
